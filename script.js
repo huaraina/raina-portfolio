afb3d1e3ea08732f3c717490362ff24d72e687c6
huaraina <95729626+huaraina@users.noreply.github.com>
2023-12-07 12:30:46 -0500

    tell user that the site is going dormant.

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// nothing yet fr
+window.addEventListener("load", (event) => {
+    console.log("why are you looking here.");
+    alert("hey, this is my final te11 project for the year. more next year hopefully. i will probably continue using this site for other things though. probably for a site for writeups on other projects. not sure.");
+});

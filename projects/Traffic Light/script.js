1c95f0916933965b91533e678477e779cfdd81fe
huaraina <95729626+huaraina@users.noreply.github.com>
2024-01-11 09:34:28 -0500

    Set up world history project

--- projects/Traffic Light/script.js
+++ projects/Traffic Light/script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", (event) => {
-    console.log("why are you looking here.");
-    alert("hey, this specific page is not my final te11 project for the year, pbm bridge is. more next year hopefully. i will probably continue using this site for other things though. probably for a site for writeups on other projects. not sure.");
-});
+//window.addEventListener("load", (event) => {
+//    console.log("why are you looking here.");
+//    alert("hey, this specific page is not my final te11 project for the year, pbm bridge is. more next year hopefully. i will probably continue using this site for other things though. probably for a site for writeups on other projects. not sure.");
+//});

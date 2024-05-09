e51084eec465fc86130b238772e470c4c97df31d
Raina <95729626+huaraina@users.noreply.github.com>
2024-05-09 11:50:21 -0400

    yay

---
+++ projects/q4writing/entries/script.js
@@ -0,0 +1,5 @@
+// just a simple js that i used on the other pages of my normal site.
+//window.addEventListener("load", (event) => {
+    //console.log("why are you looking here.");
+    //alert("hey, this specific page is not my final te11 project for the year, pbm bridge is. more next year hopefully. i will probably continue using this site for other things though. probably for a site for writeups on other projects. not sure.");
+//});
